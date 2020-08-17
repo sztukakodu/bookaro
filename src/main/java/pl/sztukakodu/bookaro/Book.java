3af88adf948ef65287f04493a8e6b788eb5223b0
Dariusz Mydlarz <dmydlarz@gmail.com>
2020-08-17 09:35:55 +0200

    M2L4 - Dodanie pierwszego beana Springowego

---
+++ src/main/java/pl/sztukakodu/bookaro/Book.java
@@ -0,0 +1,27 @@
+package pl.sztukakodu.bookaro;
+
+import java.util.StringJoiner;
+
+class Book {
+    Long id;
+    String title;
+    String author;
+    Integer year;
+
+    public Book(Long id, String title, String author, Integer year) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Book.class.getSimpleName() + "[", "]")
+            .add("id=" + id)
+            .add("title='" + title + "'")
+            .add("author='" + author + "'")
+            .add("year=" + year)
+            .toString();
+    }
+}
---
+++ src/test/java/pl/sztukakodu/bookaro/BookaroOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sztukakodu.bookaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookaroOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
