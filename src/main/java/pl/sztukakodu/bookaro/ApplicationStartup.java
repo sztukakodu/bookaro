1572a955d2fc42cf50f1f63219ead407cea977bb
Dariusz Mydlarz <dmydlarz@gmail.com>
2020-08-31 12:32:09 +0200

    M2L12 - Wstrzykiwanie primitivow

--- src/main/java/pl/sztukakodu/bookaro/ApplicationStartup.java
+++ src/main/java/pl/sztukakodu/bookaro/ApplicationStartup.java
@@ -1,6 +1,7 @@
 package pl.sztukakodu.bookaro;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pl.sztukakodu.bookaro.catalog.application.CatalogController;
@@ -9,14 +10,25 @@ import pl.sztukakodu.bookaro.catalog.domain.Book;
 import java.util.List;
 
 @Component
-@RequiredArgsConstructor
 class ApplicationStartup implements CommandLineRunner {
 
     private final CatalogController catalogController;
+    private final String title;
+    private final Long limit;
+
+    public ApplicationStartup(
+        CatalogController catalogController,
+        @Value("${bookaro.catalog.query}") String title,
+        @Value("${bookaro.catalog.limit}") Long limit
+    ) {
+        this.catalogController = catalogController;
+        this.title = title;
+        this.limit = limit;
+    }
 
     @Override
     public void run(String... args) {
-        List<Book> books = catalogController.findByTitle("Pan");
-        books.forEach(System.out::println);
+        List<Book> books = catalogController.findByTitle(title);
+        books.stream().limit(limit).forEach(System.out::println);
     }
 }
