02dfdd25cba17f922c485aa457b0873a5c21b7b0
Dariusz Mydlarz <dmydlarz@gmail.com>
2020-12-07 15:22:43 +0100

    M8L10 - Testowanie warstwy API (TestRestTemplate)

---
+++ src/test/java/pl/sztukakodu/bookaro/catalog/web/CatalogControllerApiTest.java
@@ -0,0 +1,51 @@
+package pl.sztukakodu.bookaro.catalog.web;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import pl.sztukakodu.bookaro.catalog.application.port.CatalogUseCase;
+import pl.sztukakodu.bookaro.catalog.domain.Book;
+
+import java.math.BigDecimal;
+import java.net.URI;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class CatalogControllerApiTest {
+
+    @LocalServerPort
+    private int port;
+
+    @MockBean
+    CatalogUseCase catalogUseCase;
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    public void getAllBooks() {
+        // given
+        Book effective = new Book("Effective Java", 2005, new BigDecimal("99.00"), 50L);
+        Book concurrency = new Book("Java Concurrency", 2006, new BigDecimal("99.00"), 50L);
+        Mockito.when(catalogUseCase.findAll()).thenReturn(List.of(effective, concurrency));
+        ParameterizedTypeReference<List<Book>> type = new ParameterizedTypeReference<>() {
+        };
+
+        // when
+        RequestEntity<Void> request = RequestEntity.get(URI.create("http://localhost:" + port + "/catalog")).build();
+        ResponseEntity<List<Book>> response = restTemplate.exchange(request, type);
+
+        // then
+        assertEquals(2, response.getBody().size());
+    }
+}
