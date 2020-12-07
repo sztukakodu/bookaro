115abeafb4add2b997bd967b0c3c4ccdad8be649
Dariusz Mydlarz <dmydlarz@gmail.com>
2020-12-07 11:35:07 +0100

    M8L5 - Dane testowe

--- src/test/java/pl/sztukakodu/bookaro/catalog/web/CatalogControllerIT.java
+++ src/test/java/pl/sztukakodu/bookaro/catalog/web/CatalogControllerIT.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import pl.sztukakodu.bookaro.catalog.application.port.CatalogUseCase;
 import pl.sztukakodu.bookaro.catalog.application.port.CatalogUseCase.CreateBookCommand;
 import pl.sztukakodu.bookaro.catalog.db.AuthorJpaRepository;
@@ -15,10 +17,11 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 class CatalogControllerIT {
 
     @Autowired
@@ -33,7 +36,31 @@ class CatalogControllerIT {
     @Test
     public void getAllBooks() {
         // given
-        Author goetz = authorJpaRepository.save(new Author("Brian Goetz"));
+        givenEffectiveJava();
+        givenJavaConcurrencyInPractice();
+
+        // when
+        List<Book> all = controller.getAll(Optional.empty(), Optional.empty());
+
+        // then
+        assertEquals(2, all.size());
+    }
+
+    @Test
+    public void getBooksByAuthor() {
+        // given
+        givenEffectiveJava();
+        givenJavaConcurrencyInPractice();
+
+        // when
+        List<Book> all = controller.getAll(Optional.empty(), Optional.of("Bloch"));
+
+        // then
+        assertEquals(1, all.size());
+        assertEquals("Effective Java", all.get(0).getTitle());
+    }
+
+    private void givenEffectiveJava() {
         Author bloch = authorJpaRepository.save(new Author("Joshua Bloch"));
         catalogUseCase.addBook(new CreateBookCommand(
             "Effective Java",
@@ -42,6 +69,10 @@ class CatalogControllerIT {
             new BigDecimal("99.90"),
             50L
         ));
+    }
+
+    private void givenJavaConcurrencyInPractice() {
+        Author goetz = authorJpaRepository.save(new Author("Brian Goetz"));
         catalogUseCase.addBook(new CreateBookCommand(
             "Java Concurrency in Practice",
             Set.of(goetz.getId()),
@@ -49,12 +80,6 @@ class CatalogControllerIT {
             new BigDecimal("129.90"),
             50L
         ));
-
-        // when
-        List<Book> all = controller.getAll(Optional.empty(), Optional.empty());
-
-        // then
-        assertEquals(2, all.size());
     }
 
 }
