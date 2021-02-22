d9967a754ead0ed39926f1b1ad90837913bf84cc
Dariusz Mydlarz <dmydlarz@gmail.com>
2021-02-22 09:58:07 +0100

    M8L12 - Test - moge zlozyc zamowienie a lista dostepnych ksiazek spada

--- src/test/java/pl/sztukakodu/bookaro/order/application/ManipulateOrderServiceTest.java
+++ src/test/java/pl/sztukakodu/bookaro/order/application/ManipulateOrderServiceTest.java
@@ -2,8 +2,10 @@ package pl.sztukakodu.bookaro.order.application;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import pl.sztukakodu.bookaro.catalog.application.port.CatalogUseCase;
 import pl.sztukakodu.bookaro.catalog.db.BookJpaRepository;
 import pl.sztukakodu.bookaro.catalog.domain.Book;
 import pl.sztukakodu.bookaro.order.application.port.ManipulateOrderUseCase.OrderItemCommand;
@@ -13,11 +15,11 @@ import pl.sztukakodu.bookaro.order.domain.Recipient;
 
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
-@Import({ManipulateOrderService.class})
+@SpringBootTest
+@AutoConfigureTestDatabase
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ManipulateOrderServiceTest {
 
     @Autowired
@@ -25,6 +27,9 @@ class ManipulateOrderServiceTest {
 
     @Autowired
     ManipulateOrderService service;
+
+    @Autowired
+    CatalogUseCase catalogUseCase;
 
     @Test
     public void userCanPlaceOrder() {
@@ -34,7 +39,7 @@ class ManipulateOrderServiceTest {
         PlaceOrderCommand command = PlaceOrderCommand
             .builder()
             .recipient(recipient())
-            .item(new OrderItemCommand(effectiveJava.getId(), 10))
+            .item(new OrderItemCommand(effectiveJava.getId(), 15))
             .item(new OrderItemCommand(jcip.getId(), 10))
             .build();
 
@@ -43,6 +48,8 @@ class ManipulateOrderServiceTest {
 
         // then
         assertTrue(response.isSuccess());
+        assertEquals(35L, catalogUseCase.findById(effectiveJava.getId()).get().getAvailable());
+        assertEquals(40L, catalogUseCase.findById(jcip.getId()).get().getAvailable());
     }
 
     @Test
