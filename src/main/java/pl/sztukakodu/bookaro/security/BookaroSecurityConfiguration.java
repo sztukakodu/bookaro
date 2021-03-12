4dde5dfb612b0ef2bb66de95597342cd05e6d047
Dariusz Mydlarz <dmydlarz@gmail.com>
2021-03-12 08:19:20 +0100

    M9L11 - Polaczenie security z baza danych

--- src/main/java/pl/sztukakodu/bookaro/security/BookaroSecurityConfiguration.java
+++ src/main/java/pl/sztukakodu/bookaro/security/BookaroSecurityConfiguration.java
@@ -1,26 +1,38 @@
 package pl.sztukakodu.bookaro.security;
 
+import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import pl.sztukakodu.bookaro.user.db.UserEntityRepository;
 
 import java.util.List;
 
+@AllArgsConstructor
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableConfigurationProperties(AdminConfig.class)
 class BookaroSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final UserEntityRepository userEntityRepository;
+    private final AdminConfig config;
 
     @Bean
     User systemUser() {
-        return new User("systemUser", "", List.of(new SimpleGrantedAuthority("ROLE_ADMIN")));
+        return config.adminUser();
     }
 
     @Override
@@ -45,14 +57,21 @@ class BookaroSecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-            .withUser("marek@example.org")
-            .password("{noop}xxx")
-            .roles("USER")
-            .and()
-            .withUser("admin")
-            .password("{noop}xxx")
-            .roles("ADMIN");
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        BookaroUserDetailsService detailsService = new BookaroUserDetailsService(userEntityRepository, config);
+        provider.setUserDetailsService(detailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/pl/sztukakodu/bookaro/security/BookaroUserDetailsService.java
+++ src/main/java/pl/sztukakodu/bookaro/security/BookaroUserDetailsService.java
@@ -10,9 +10,13 @@ import pl.sztukakodu.bookaro.user.db.UserEntityRepository;
 public class BookaroUserDetailsService implements UserDetailsService {
 
     private final UserEntityRepository repository;
+    private final AdminConfig config;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if(config.getUsername().equalsIgnoreCase(username)) {
+            return config.adminUser();
+        }
         return repository.findByUsernameIgnoreCase(username)
                          .map(UserEntityDetails::new)
                          .orElseThrow(() -> new UsernameNotFoundException(username));
